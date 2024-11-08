c0dc42dbd74dde40061587d0a1c7b21ca49e915c
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-11-08 17:36:22 +0600

    complete peoblem-7

---
+++ src/problem-7.ts
@@ -0,0 +1,13 @@
+{
+    //
+    class Car {
+        constructor(public make:string, public model:string, public year: number){
+        }
+        getCarAge(): number{
+            const currentYear = new Date().getFullYear()
+            return currentYear - this.year
+        }
+    }
+
+    //
+}
